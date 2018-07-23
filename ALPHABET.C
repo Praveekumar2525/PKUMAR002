72ee73e3e6694e3fe87e90653430469b6a6c48ba
Praveekumar2525 <praveen13kumar96@gmail.com>
2018-07-23 11:22:16 +0530

    Update ALPHABET.C

--- ALPHABET.C
+++ ALPHABET.C
@@ -1,9 +1,11 @@
 #include<stdio.h> 
 int main(void) {
+  voidmain();
 char x;
 printf("\n enter the character to check:");
 scanf("%c",&x);
-if(x>='a' && x<='z' || x>='A' && x<='Z') {
+if(x>='a' && x<='z' || x>='A' && x<='Z') 
+{
 printf("\nyes %c is an alphabet",x); }
 else
 printf("\n no it is not an alphabet");
