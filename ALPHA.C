6716c8cfd0e6af96bc0e611ed216f7bc24718e6b
Praveekumar2525 <praveen13kumar96@gmail.com>
2018-07-23 12:11:43 +0530

    Create ALPHA.C

---
+++ ALPHA.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	char x;
+	printf("Enter the character:");
+	scanf("%c",&x);
+	if((x>='a'&&x<='z')||(x>='A'&&x<='Z'))
+	{
+		printf("character");
+	}
+	else
+	{
+printf("not a character");
+	}
+}
+getch();
